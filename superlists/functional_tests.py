7338b837d45871ec726957640e44c0288ab5c85f
elaimaz <fariawesley4@gmail.com>
2019-04-11 14:28:50 -0300

    Functional test now checks we can input a to-do

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -9,31 +18,47 @@ class NewVisitorTest(unittest.TestCase):
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith ouviu falar de uma nova aplicação online interessante para
+        # Edith ouviu falar de uma nova aplicacao online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
         self.browser.get('http://localhost:8000')
 
 
-        # Ela percebe que o título da página e o cabeçalho mencionam listas de
+        # Ela percebe que o título da página e o cabecalho mencionam listas de
         # tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
 
         # Ela é convidada a inserir um item de tarefa imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
 
         # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
         # de texto (o hobby de Edith é fazer iscas para pesca com fly)
+        input.send_keys('Buy peacock feathers')
 
 
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        input.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
         # item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão
         # para fazer um fly - Edith é bem metódica)
+        self.fail('Finish the test!')
 
 
         # A pagina é atualizada novamente e agora mostra os dois itens em sua lista
