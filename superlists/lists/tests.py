95c3dca7968e0d823e8111b55dfade10bc612ac6
elaimaz <fariawesley4@gmail.com>
2019-04-11 15:14:50 -0300

    Refactor home page view to use a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -9,14 +10,8 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/') # resolve é uma função do Django para resolver URLs e descobrir para qual view eles devem ser mapeados.
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest() # Cria um objeto HttpRequest
-        response = home_page(request) #é passado para a view home_page, que dara uma resposta
-        html = response.content.decode('utf8') # É extraido o .content da resposta (os bytes brutos). .decode() converte os bytes em string HTML
-        self.assertTrue(html.startswith('<html>')) #Testa se começa com uma tah <html>
-        self.assertIn('<title>To-Do lists</title>', html) # testa se tem a tag <title> contendo as palavras To-Do lists
-        self.assertTrue(html.endswith('</html>')) #Testa se termina com tag </html>
+
